fc98b2e2da011a25d9295ef0ed4b808c10b53fc4
sheinDihayco <shiendihayco16@gmail.com>
2024-07-22 21:55:07 +0800

    db

--- db/schooldb.sql
+++ db/schooldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2024 at 04:48 AM
+-- Generation Time: Jul 22, 2024 at 03:53 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
