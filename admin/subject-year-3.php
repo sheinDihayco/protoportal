eb7981dc8f887a2a94f1ce06c911166928911be4
sheinDihayco <shiendihayco16@gmail.com>
2024-08-01 17:05:54 +0800

    adding subjects

--- admin/subject-year-3.php
+++ admin/subject-year-3.php
@@ -31,8 +31,6 @@
                 <option>BSIT</option>
                 <option>BSBA</option>
                 <option>BSOA</option>
-                <option>BSIT-SUMMER</option>
-
               </select>
               <div class="invalid-feedback">
                 Please select a valid course.
@@ -56,7 +54,6 @@
               <label for="semester" class="form-label">Semester</label>
               <select class="form-select" id="semester" name="semester" required>
                 <option selected disabled value="">Choose...</option>
-                <option>0</option>
                 <option>1</option>
                 <option>2</option>
               </select>
@@ -146,7 +143,7 @@
             </div>
 
             <div class="card-body">
-              <h5 class="card-title">Course <span>| BSIT - 1 </span></h5>
+              <h5 class="card-title">Course <span>| BSIT - 3 </span></h5>
 
               <table class="table table-striped datatable">
                 <thead>
@@ -169,7 +166,7 @@
                   $db = $database->open();
 
                   try {
-                    $sql = "SELECT * FROM tbl_subjects WHERE course = 'BSIT' AND year = '1' ";
+                    $sql = "SELECT * FROM tbl_subjects WHERE (course = 'BSIT' OR course = 'BSIT-SUMMER') AND year = '3'";
                     foreach ($db->query($sql) as $subject) {
                   ?>
                       <tr>
@@ -226,7 +223,7 @@
             </div>
 
             <div class="card-body">
-              <h5 class="card-title">Subject <span>| BSOA - 1</span></h5>
+              <h5 class="card-title">Subject <span>| BSOA - 3 </span></h5>
 
               <table class="table table-striped datatable">
                 <thead>
@@ -249,7 +246,7 @@
                   $db = $database->open();
 
                   try {
-                    $sql = "SELECT * FROM tbl_subjects WHERE course = 'BSOA' AND year = '1' ";
+                    $sql = "SELECT * FROM tbl_subjects WHERE course = 'BSOA' AND year = '3' ";
                     foreach ($db->query($sql) as $subject) {
                   ?>
                       <tr>
@@ -306,7 +303,7 @@
             </div>
 
             <div class="card-body">
-              <h5 class="card-title">Subject <span>| BSBA - 1 </span></h5>
+              <h5 class="card-title">Subject <span>| BSBA - 3 </span></h5>
 
               <table class="table table-striped datatable">
                 <thead>
@@ -329,7 +326,7 @@
                   $db = $database->open();
 
                   try {
-                    $sql = "SELECT * FROM tbl_subjects WHERE course = 'BSBA' AND year = '1' ";
+                    $sql = "SELECT * FROM tbl_subjects WHERE course = 'BSBA' AND year = '3' ";
                     foreach ($db->query($sql) as $subject) {
                   ?>
                       <tr>
@@ -368,164 +365,7 @@
       </div>
     </div>
 
-    <div class="col-lg-12">
-      <div class="row">
-        <div class="col-12">
-          <div class="card recent-sales overflow-auto">
-            <div class="filter">
-              <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
-                <li><a class="dropdown-item" href="subject.php">1</a></li>
-                <li><a class="dropdown-item" href="subject-year-2.php">2</a></li>
-                <li><a class="dropdown-item" href="subject-year-3.php">3</a></li>
-                <li><a class="dropdown-item" href="subject-year-4.php">4</a></li>
-              </ul>
-            </div>
 
-            <div class="card-body">
-              <h5 class="card-title">Subject <span>| Grade 11 </span></h5>
-
-              <table class="table table-striped datatable">
-                <thead>
-                  <tr>
-                    <th scope="col">Semester</th>
-                    <th scope="col">Code</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Lec</th>
-                    <th scope="col">Lab</th>
-                    <th scope="col">Units</th>
-                    <th scope="col">Pre-requisite</th>
-                    <th scope="col">Total Hours</th>
-                    <th scope="col">Course</th>
-                    <th scope="col">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php
-                  $database = new Connection();
-                  $db = $database->open();
-
-                  try {
-                    $sql = "SELECT * FROM tbl_subjects  WHERE course = 'GRADE11'";
-                    foreach ($db->query($sql) as $subject) {
-                  ?>
-                      <tr>
-                        <td><?php echo htmlspecialchars($subject['semester']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['code']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['description']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['lec']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['lab']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['unit']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['pre_req']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['total']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['course']); ?></td>
-                        <td>
-                          <button type="button" class="btn btn-sm btn-warning ri-edit-2-fill" data-bs-toggle="modal" data-bs-target="#editStudent<?php echo $subject['id']; ?>"></button>
-                          <form method="POST" action="../admin/upload/delete_subject.php" onsubmit="return confirm('Are you sure you want to delete this subject?');" style="display:inline;">
-                            <input type="hidden" name="id" value="<?php echo htmlspecialchars($subject['id']); ?>">
-                            <button type="submit" class="btn btn-sm btn-danger ri-delete-bin-6-line"></button>
-                          </form>
-
-                        </td>
-                        <?php include('modals/form-edit-Student.php'); ?>
-                      </tr>
-                  <?php
-                    }
-                  } catch (PDOException $e) {
-                    echo "There is some problem in connection: " . $e->getMessage();
-                  }
-                  $database->close();
-                  ?>
-                </tbody>
-              </table>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-12">
-      <div class="row">
-        <div class="col-12">
-          <div class="card recent-sales overflow-auto">
-            <div class="filter">
-              <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
-                <li><a class="dropdown-item" href="subject.php">1</a></li>
-                <li><a class="dropdown-item" href="subject-year-2.php">2</a></li>
-                <li><a class="dropdown-item" href="subject-year-3.php">3</a></li>
-                <li><a class="dropdown-item" href="subject-year-4.php">4</a></li>
-              </ul>
-            </div>
-
-            <div class="card-body">
-              <h5 class="card-title">Subject <span>| Grade 12 </span></h5>
-
-              <table class="table table-striped datatable">
-                <thead>
-                  <tr>
-                    <th scope="col">Semester</th>
-                    <th scope="col">Code</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Lec</th>
-                    <th scope="col">Lab</th>
-                    <th scope="col">Units</th>
-                    <th scope="col">Pre-requisite</th>
-                    <th scope="col">Total Hours</th>
-                    <th scope="col">Course</th>
-                    <th scope="col">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php
-                  $database = new Connection();
-                  $db = $database->open();
-
-                  try {
-                    $sql = "SELECT * FROM tbl_subjects  WHERE course = 'GRADE12'";
-                    foreach ($db->query($sql) as $subject) {
-                  ?>
-                      <tr>
-                        <td><?php echo htmlspecialchars($subject['semester']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['code']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['description']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['lec']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['lab']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['unit']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['pre_req']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['total']); ?></td>
-                        <td><?php echo htmlspecialchars($subject['course']); ?></td>
-                        <td>
-                          <button type="button" class="btn btn-sm btn-warning ri-edit-2-fill" data-bs-toggle="modal" data-bs-target="#editStudent<?php echo $subject['id']; ?>"></button>
-                          <form method="POST" action="../admin/upload/delete_subject.php" onsubmit="return confirm('Are you sure you want to delete this subject?');" style="display:inline;">
-                            <input type="hidden" name="id" value="<?php echo htmlspecialchars($subject['id']); ?>">
-                            <button type="submit" class="btn btn-sm btn-danger ri-delete-bin-6-line"></button>
-                          </form>
-
-                        </td>
-                        <?php include('modals/form-edit-Student.php'); ?>
-                      </tr>
-                  <?php
-                    }
-                  } catch (PDOException $e) {
-                    echo "There is some problem in connection: " . $e->getMessage();
-                  }
-                  $database->close();
-                  ?>
-                </tbody>
-              </table>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
   </section>
 </main>
 
