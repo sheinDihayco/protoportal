e37200fb4242beae044be4b3a8b141b31d9a35cf
sheinDihayco <shiendihayco16@gmail.com>
2024-08-19 20:53:38 +0800

    index-update

--- admin/index.php
+++ admin/index.php
@@ -3,9 +3,6 @@ include_once "../templates/header.php";
 include_once "includes/connect.php";
 include_once 'includes/connection.php';
 
-$statements = $conn->prepare("SELECT SUM(netpay) AS total_netpay FROM tbl_transaction");
-$statements->execute();
-$totalnet = $statements->fetch(PDO::FETCH_ASSOC);
 
 $statements = $conn->prepare("SELECT COUNT(employee_id) AS count_emp FROM tbl_employee");
 $statements->execute();
@@ -87,34 +84,6 @@ $studcount = $statements->fetch(PDO::FETCH_ASSOC);
               </div>
             </div>
           </div><!-- End Student Count Card -->
-
-          <!-- Revenue Card -->
-          <div class="col-xxl-4 col-md-6">
-            <div class="card info-card revenue-card">
-              <div class="filter">
-                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                  <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                  </li>
-                  <li><a class="dropdown-item" href="#">Today</a></li>
-                  <li><a class="dropdown-item" href="#">This Month</a></li>
-                  <li><a class="dropdown-item" href="#">This Year</a></li>
-                </ul>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">Total Net <span>| This Year</span></h5>
-                <div class="d-flex align-items-center">
-                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-currency-dollar"></i>
-                  </div>
-                  <div class="ps-3">
-                    <h6><?php echo number_format($totalnet['total_netpay'], 2, '.', ',') ?></h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div><!-- End Revenue Card -->
 
           <!-- Students Enrolled -->
           <div class="col-12">
